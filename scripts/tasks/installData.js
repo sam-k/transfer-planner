aa5dfb1a8c8c65d71d2505a1aab9b2ed4f440e1f
Sam Kim <21956685+sam-k@users.noreply.github.com>
2024-01-29 01:34:11 -0500

    JSON schemas, install & build OTP, etc. (#3)

--- scripts/tasks/installData.js
+++ scripts/tasks/installData.js
@@ -1,53 +1,36 @@
 import chalk from 'chalk';
-import {createWriteStream, readFileSync} from 'fs';
-import {get as getHttp} from 'http';
-import {get as getHttps} from 'https';
+import {readFileSync} from 'fs';
 import minimist from 'minimist';
+import {join as joinPath} from 'path';
 
-import {printUsage} from '../utils.js';
+import {
+  DownloadError,
+  downloadFromUrl,
+  getAbsolutePath,
+  handleError,
+  printInfo,
+  printUsage,
+} from '../utils/index.js';
 
-const DATA_SOURCES_CONFIG_PATH = 'config/dataSources.json';
-const SECRETS_CONFIG_PATH = 'config/secrets.json';
+const DATA_DIR = getAbsolutePath('data');
+
+const DATA_SOURCES_CONFIG_PATH = getAbsolutePath('config/dataSources.json');
+const SECRETS_CONFIG_PATH = getAbsolutePath('config/secrets.json');
 
 /**
- * Downloads data from a URL.
+ * Downloads data specified by data source group IDs.
  *
- * @param {string} url Download URL
- * @param {string} outPath File path to write the downloaded data
- * @returns {Promise<void>}
+ * @param {string[]} groupIds Data source group IDs, or all IDs if unspecified
  */
-const downloadData = (url, outPath) =>
-  new Promise((resolve, reject) => {
-    const pipeFn = (/** @type {import('http').IncomingMessage} */ res) => {
-      res.pipe(createWriteStream(outPath));
-    };
-
-    const req = url.startsWith('https://')
-      ? getHttps(url, pipeFn)
-      : getHttp(url, pipeFn);
-    req.on('error', err => {
-      reject(err);
-    });
-    resolve();
-  });
-
-const main = async () => {
-  const argv = minimist(process.argv.slice(2));
-  if (argv.h || argv.help) {
-    printUsage(['installData [data-source-group-ids]...', 'installData'], {
-      '-h, --help': 'Display usage information',
-    });
-    return;
-  }
-  const dataSourceGroupIds = new Set(argv._);
-
+const downloadDataGroups = async groupIds => {
   /** @type {import('../../config/schemas').DataSources} */
   const dataSourcesConfig = JSON.parse(
     readFileSync(DATA_SOURCES_CONFIG_PATH).toString()
   );
-  if (dataSourceGroupIds.size) {
+  if (groupIds.length) {
+    const groupIdSet = new Set(groupIds);
     dataSourcesConfig.groups = dataSourcesConfig.groups.filter(group =>
-      dataSourceGroupIds.has(group.id)
+      groupIdSet.has(group.id)
     );
   }
 
@@ -59,39 +42,78 @@ const main = async () => {
     secretsConfig.secrets.map(secret => [secret.id, secret.secret])
   );
 
-  const logMsg = dataSourcesConfig.groups
-    .map(group =>
-      [
-        `Fetching data for: ${chalk.magenta(
-          chalk.bold(group.id) +
-            (group.description ? ` (${group.description})` : '')
-        )}...`,
-        ...group.sources.map(
-          source =>
-            `- ${source.id}` +
-            (source.description ? ` (${source.description})` : '')
-        ),
-      ].join('\n')
-    )
-    .join('\n\n');
-  console.log(logMsg);
+  for (const group of dataSourcesConfig.groups) {
+    printInfo(
+      `Fetching data for: ${
+        chalk.bold(group.id) +
+        (group.description ? ` (${group.description})` : '')
+      }...`,
+      ...group.sources.map(
+        source =>
+          `- ${source.id}` +
+          (source.description ? ` (${source.description})` : '')
+      )
+    );
+  }
 
   await Promise.all(
     dataSourcesConfig.groups
-      .flatMap(group => group.sources)
+      .flatMap(group =>
+        group.sources.map(source => ({
+          groupId: group.id,
+          ...source,
+        }))
+      )
       .map(source => {
+        const unifiedId = `${source.groupId}:${source.id}`;
         if (!source.fileName) {
-          throw `Filename for ${source.id} not provided.`;
+          throw new DownloadError(
+            unifiedId,
+            'Filename not provided in data source.'
+          );
         }
-        return downloadData(
-          source.url.replace(
-            '${SECRET}',
-            secrets.get(source.secretId ?? '') ?? ''
-          ),
-          source.fileName
-        );
-      }) ?? []
+
+        let dataUrl = source.url;
+        if (dataUrl.includes('${SECRET}')) {
+          if (!source.secretId) {
+            throw new DownloadError(
+              unifiedId,
+              'Secret ID not provided in data source URL.'
+            );
+          }
+          const secret = secrets.get(source.secretId);
+          if (!secret) {
+            throw new DownloadError(
+              unifiedId,
+              `Secret not found for ID ${source.secretId}.`
+            );
+          }
+          dataUrl = dataUrl.replace('${SECRET}', secret);
+        }
+
+        return downloadFromUrl({
+          name: unifiedId,
+          url: dataUrl,
+          outAbsolutePath: joinPath(DATA_DIR, source.fileName),
+        });
+      })
   );
+};
+
+const main = async () => {
+  const argv = minimist(process.argv.slice(2));
+  if (argv.h || argv.help) {
+    printUsage(['installData [data-source-group-ids]...', 'installData'], {
+      '-h, --help': 'Display usage information',
+    });
+    return;
+  }
+
+  try {
+    await downloadDataGroups(argv._);
+  } catch (err) {
+    handleError(err);
+  }
 };
 
 main();
---
+++ scripts/utils/index.js
@@ -0,0 +1,5 @@
+export * from './console.js';
+export * from './download.js';
+export * from './errors.js';
+export * from './log.js';
+export * from './utils.js';
