c36e4e479c97cc0d2fd7cf1232a12a6c85726417
Sam Kim <21956685+sam-k@users.noreply.github.com>
2024-01-31 06:35:42 -0800

    Map, etc

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,18 @@
 import React from 'react';
 
-const App = () => <></>;
+import BaseMap from './BaseMap';
+
+const App = () => (
+  <>
+    <BaseMap
+      tileServer="osm"
+      // TODO: Read from config instead of hardcoding.
+      boundingBox={[
+        [36.791, -123.64],
+        [38.719, -121.025],
+      ]}
+    />
+  </>
+);
 
 export default App;
